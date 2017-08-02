7aa7069ff25eee3e1115bbd9cd4a3859a455ff90
2liang <2liang@2liangdeMacBook-Pro.local>
2017-08-02 16:16:04 +0800

    fix

--- routers/base.go
+++ routers/base.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	//"github.com/2liang/mcache/controller"
+	"github.com/2liang/mcache/controller"
 )
 
 func Init(r *gin.Engine) {
@@ -18,41 +19,41 @@ func Init(r *gin.Engine) {
 	})
 
 	// 项目
-	//project_r := r.Group("/project")
-	//{
-	//	// 获取项目
-	//	project_r.GET("/get", controller.GetProject)
-	//	// 添加项目
-	//	project_r.POST("/add", controller.AddProject)
-	//	// 更新项目
-	//	project_r.POST("/update", controller.UpdateProject)
-	//	// 删除项目
-	//	project_r.POST("/delete", controller.DeleteProject)
-	//}
-	//
-	//// 实例
-	//case_r := r.Group("/case")
-	//{
-	//	// 获取实例
-	//	case_r.GET("/get", controller.GetCase)
-	//	// 添加实例
-	//	case_r.POST("/add", controller.AddCase)
-	//	// 更新实例
-	//	case_r.POST("/update", controller.UpdateCase)
-	//	// 删除实例
-	//	case_r.POST("/delete", controller.DeleteCase)
-	//}
-	//
-	//// key
-	//key_r := r.Group("/key")
-	//{
-	//	// 获取key
-	//	key_r.GET("/get", controller.GetKey)
-	//	// 添加key
-	//	key_r.POST("/add", controller.AddKey)
-	//	// 更新key
-	//	key_r.POST("/update", controller.UpdateKey)
-	//	// 删除key
-	//	key_r.POST("/delete", controller.DeleteKey)
-	//}
+	project_r := r.Group("/project")
+	{
+		// 获取项目
+		project_r.GET("/get", controller.GetProject)
+		// 添加项目
+		project_r.POST("/add", controller.AddProject)
+		// 更新项目
+		project_r.POST("/update", controller.UpdateProject)
+		// 删除项目
+		project_r.POST("/delete", controller.DeleteProject)
+	}
+
+	// 实例
+	case_r := r.Group("/case")
+	{
+		// 获取实例
+		case_r.GET("/get", controller.GetCase)
+		// 添加实例
+		case_r.POST("/add", controller.AddCase)
+		// 更新实例
+		case_r.POST("/update", controller.UpdateCase)
+		// 删除实例
+		case_r.POST("/delete", controller.DeleteCase)
+	}
+
+	// key
+	key_r := r.Group("/key")
+	{
+		// 获取key
+		key_r.GET("/get", controller.GetKey)
+		// 添加key
+		key_r.POST("/add", controller.AddKey)
+		// 更新key
+		key_r.POST("/update", controller.UpdateKey)
+		// 删除key
+		key_r.POST("/delete", controller.DeleteKey)
+	}
 }
